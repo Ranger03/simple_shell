fdd95414aee532b0eddbbc3958dcf66431d618c6
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-05-25 14:13:00 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,10 +1,13 @@
 #include "shell.h"
 /**
- * main - initialize the variables of the program
- * @argc: number of values received from the command line
- * @argv: values received from the command line
- * @env: number of values received from the command line
- * Return: zero on succes.
+ * main - sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * @argc: numbsdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkmand line
+ * @argv: vasdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk line
+ * @env: nsdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkmand line
+ * Return: sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk
  */
 int main(int argc, char *argv[], char *env[])
 {
@@ -16,8 +19,8 @@ int main(int argc, char *argv[], char *env[])
 	signal(SIGINT, handle_ctrl_c);
 
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
-	{/* We are in the terminal, interactive mode */
-		errno = 2;/*???????*/
+	{/*sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk */
+		errno = 2;/*sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk*/
 		prompt = PROMPT_MSG;
 	}
 	errno = 0;
@@ -26,9 +29,11 @@ int main(int argc, char *argv[], char *env[])
 }
 
 /**
- * handle_ctrl_c - print the prompt in a new line
- * when the signal SIGINT (ctrl + c) is send to the program
- * @UNUSED: option of the prototype
+ * handle_ctrl_c - sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkline
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * @UNUSED: sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk
  */
 void handle_ctrl_c(int opr UNUSED)
 {
@@ -37,11 +42,11 @@ void handle_ctrl_c(int opr UNUSED)
 }
 
 /**
- * inicialize_data - inicialize the struct with the info of the program
- * @data: pointer to the structure of data
- * @argv: array of arguments pased to the program execution
- * @env: environ pased to the program execution
- * @argc: number of values received from the command line
+ * inicialize_data - insdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkgram
+ * @data: psdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk  ata
+ * @argv: arraysdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk execution
+ * @env: envirosdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk ion
+ * @argc: number sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldk mand line
  */
 void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
@@ -51,7 +56,7 @@ void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 	data->input_line = NULL;
 	data->command_name = NULL;
 	data->exec_counter = 0;
-	/* define the file descriptor to be readed*/
+	/* sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk */
 	if (argc == 1)
 		data->file_descriptor = STDIN_FILENO;
 	else
@@ -85,9 +90,10 @@ void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 	}
 }
 /**
- * sisifo - its a infinite loop that shows the prompt
- * @prompt: prompt to be printed
- * @data: its a infinite loop that shows the prompt
+ * sisifo - sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk
+ * whesdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjaseldkprogram
+ * @prompt: sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk
+ * @data: itssdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk prompt
  */
 void sisifo(char *prompt, data_of_program *data)
 {
@@ -101,7 +107,7 @@ void sisifo(char *prompt, data_of_program *data)
 		if (error_code == EOF)
 		{
 			free_all_data(data);
-			exit(errno); /* if EOF is the fisrt Char of string, exit*/
+			exit(errno); /* isdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldkxit*/
 		}
 		if (string_len >= 1)
 		{
@@ -109,7 +115,7 @@ void sisifo(char *prompt, data_of_program *data)
 			expand_variables(data);
 			tokenize(data);
 			if (data->tokens[0])
-			{ /* if a text is given to prompt, execute */
+			{ /* if sdf asdlkjf asdfj asdkjf l asjdf asdfj lasdjfased fjase ldk te */
 				error_code = execute(data);
 				if (error_code != 0)
 					_print_error(error_code, data);
