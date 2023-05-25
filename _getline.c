c7bf7ea40c884572c4ed58fd5b33f2729983a0bb
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-05-25 13:50:43 +0200

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -1,50 +1,50 @@
 #include "shell.h"
 
 /**
-* _getline - xczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxcv 
-* @data: stxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxm's data
+* _getline - read one line from the prompt.
+* @data: struct for the program's data
 *
-* Return: rxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxing bytes.
+* Return: reading counting bytes.
 */
 int _getline(data_of_program *data)
 {
 	char buff[BUFFER_SIZE] = {'\0'};
-	static char *array_coms[10] = {NULL};
-	static char arry_ops[10] = {'\0'};
-	ssize_t byt_red, i = 0;
+	static char *array_commands[10] = {NULL};
+	static char array_operators[10] = {'\0'};
+	ssize_t bytes_read, i = 0;
 
-	/* checkxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxst more commands in the array */
-	/* and checxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxrators */
-	if (!array_coms[0] || (arry_ops[0] == '&' && errno != 0) ||
-		(arry_ops[0] == '|' && errno == 0))
+	/* check if doesnot exist more commands in the array */
+	/* and checks the logical operators */
+	if (!array_commands[0] || (array_operators[0] == '&' && errno != 0) ||
+		(array_operators[0] == '|' && errno == 0))
 	{
-		/*free xczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zx array if it exists */
-		for (i = 0; array_coms[i]; i++)
+		/*free the memory allocated in the array if it exists */
+		for (i = 0; array_commands[i]; i++)
 		{
-			free(array_coms[i]);
-			array_coms[i] = NULL;
+			free(array_commands[i]);
+			array_commands[i] = NULL;
 		}
 
-		/* reaxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxiptor int to buff */
-		byt_red = read(data->file_descriptor, &buff, BUFFER_SIZE - 1);
-		if (byt_red == 0)
+		/* read from the file descriptor int to buff */
+		bytes_read = read(data->file_descriptor, &buff, BUFFER_SIZE - 1);
+		if (bytes_read == 0)
 			return (-1);
 
-		/* sxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxor ; */
+		/* split lines for \n or ; */
 		i = 0;
 		do {
-			array_coms[i] = str_duplicate(_strtok(i ? NULL : buff, "\n;"));
-			/*chexczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zx|| operators*/
-			i = check_logic_ops(array_coms, i, arry_ops);
-		} while (array_coms[i++]);
+			array_commands[i] = str_duplicate(_strtok(i ? NULL : buff, "\n;"));
+			/*checks and split for && and || operators*/
+			i = check_logic_ops(array_commands, i, array_operators);
+		} while (array_commands[i++]);
 	}
 
-	/*obtainsxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxt for the array*/
-	data->input_line = array_coms[0];
-	for (i = 0; array_coms[i]; i++)
+	/*obtains the next command (command 0) and remove it for the array*/
+	data->input_line = array_commands[0];
+	for (i = 0; array_commands[i]; i++)
 	{
-		array_coms[i] = array_coms[i + 1];
-		arry_ops[i] = arry_ops[i + 1];
+		array_commands[i] = array_commands[i + 1];
+		array_operators[i] = array_operators[i + 1];
 	}
 
 	return (str_length(data->input_line));
@@ -52,42 +52,42 @@ int _getline(data_of_program *data)
 
 
 /**
-* check_logic_ops - cxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zx operators
-* @array_coms: arrxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxands.
-* @i: index in thxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxbe checked
-* @arry_ops: arraxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxprevious command
+* check_logic_ops - checks and split for && and || operators
+* @array_commands: array of the commands.
+* @i: index in the array_commands to be checked
+* @array_operators: array of the logical operators for each previous command
 *
-* Return: index ofxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxrray_commands.
+* Return: index of the last command in the array_commands.
 */
-int check_logic_ops(char *array_coms[], int i, char arry_ops[])
+int check_logic_ops(char *array_commands[], int i, char array_operators[])
 {
 	char *temp = NULL;
 	int j;
 
-	/* checks fxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxd line*/
-	for (j = 0; array_coms[i] != NULL  && array_coms[i][j]; j++)
+	/* checks for the & char in the command line*/
+	for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
 	{
-		if (array_coms[i][j] == '&' && array_coms[i][j + 1] == '&')
+		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
 		{
-			/* splxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zx found */
-			temp = array_coms[i];
-			array_coms[i][j] = '\0';
-			array_coms[i] = str_duplicate(array_coms[i]);
-			array_coms[i + 1] = str_duplicate(temp + j + 2);
+			/* split the line when chars && was found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
 			i++;
-			arry_ops[i] = '&';
+			array_operators[i] = '&';
 			free(temp);
 			j = 0;
 		}
-		if (array_coms[i][j] == '|' && array_coms[i][j + 1] == '|')
+		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
 		{
-			/* splxczkjlhvszd z xcv zxcv zxclvzx cvzx cvzlxcvz xcv zxc vzxclv zxs found */
-			temp = array_coms[i];
-			array_coms[i][j] = '\0';
-			array_coms[i] = str_duplicate(array_coms[i]);
-			array_coms[i + 1] = str_duplicate(temp + j + 2);
+			/* split the line when chars || was found */
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_duplicate(array_commands[i]);
+			array_commands[i + 1] = str_duplicate(temp + j + 2);
 			i++;
-			arry_ops[i] = '|';
+			array_operators[i] = '|';
 			free(temp);
 			j = 0;
 		}
